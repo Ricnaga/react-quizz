082c9fcfbec6d986246c0b34378a9500a42ebe9f
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-12-20 18:28:10 -0300

    feat: Add params on click buttons

--- packages/frontend/angular/src/app/screens/questions/questions.component.ts
+++ packages/frontend/angular/src/app/screens/questions/questions.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Params, Router, ActivatedRoute } from '@angular/router';
+import { answers, questions } from 'src/app/config/data';
 
 const PAGE_SIZE = 1
 
@@ -6,6 +8,41 @@ const PAGE_SIZE = 1
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.scss']
 })
-export class QuestionsComponent {
 
+export class QuestionsComponent implements OnInit{
+  pageNumber = '0'
+  paginateQuestions = new Array<string>();
+  
+  answervalue = answers.map((answer, index) => ({text:answer, value:index}))
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+) { }
+
+  ngOnInit(){
+    this.activatedRoute.queryParamMap.subscribe(params => {
+      this.pageNumber = params.get('page') ?? this.pageNumber;
+    })
+  }
+
+  onPrevious(){
+    const queryParams: Params = { page: parseInt(this.pageNumber,10) - 1};
+    this.paginateQuestions = questions.slice((parseInt(this.pageNumber,10)*PAGE_SIZE),(parseInt(this.pageNumber,10)*PAGE_SIZE)+PAGE_SIZE)
+  this.router.navigate(
+    [], 
+    {      
+      queryParams, 
+    });
+  }
+  
+  onNext(){
+    const queryParams: Params = { page: parseInt(this.pageNumber,10) + 1};
+    this.paginateQuestions = questions.slice((parseInt(this.pageNumber,10)*PAGE_SIZE),(parseInt(this.pageNumber,10)*PAGE_SIZE)+PAGE_SIZE)
+  this.router.navigate(
+    [], 
+    {      
+      queryParams, 
+    });
+  }
 }
--- packages/frontend/angular/src/app/screens/questions/questions.module.ts
+++ packages/frontend/angular/src/app/screens/questions/questions.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { QuestionsComponent } from './questions.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
 
 const routes: Routes = [{ path: '', component: QuestionsComponent }]
-
 
 @NgModule({
   declarations: [QuestionsComponent],
   imports: [
     CommonModule,
     MatCardModule,
+    MatButtonModule,
+    MatRadioModule,
     RouterModule.forChild(routes)
   ]
 })
