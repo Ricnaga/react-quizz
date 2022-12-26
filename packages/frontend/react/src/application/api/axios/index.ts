8c453c5ba07af0fd062237453fd556cc143881c9
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-12-26 18:59:29 -0300

    feat: Add eslint for react

--- packages/frontend/react/src/application/api/axios/index.ts
+++ packages/frontend/react/src/application/api/axios/index.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 import { SERVER_URL } from '../../../config/environment';
 
 export const api = axios.create({
-    baseURL: SERVER_URL,
+  baseURL: SERVER_URL,
 });
--- packages/frontend/react/src/config/environment.ts
+++ packages/frontend/react/src/config/environment.ts
@@ -1,1 +1,1 @@
-export const SERVER_URL = String(import.meta.env.VITE_SERVER_URL); 
+export const SERVER_URL = String(import.meta.env.VITE_SERVER_URL);
