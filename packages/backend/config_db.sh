96cd5b5a75ad503bf9b0bb784590ac9d9360068e
Ricnaga <ric.nagato@gmail.com>
2023-02-01 16:05:55 -0300

    readme

--- packages/backend/config_db.sh
+++ packages/backend/config_db.sh
@@ -1,17 +1,34 @@
-docker logs db_quiz 2>&1 | grep GENERATED
+# Procedimento para resetar a senha do banco de dados
+# Necessário gerar uma senha randômica que o banco disponibiliza por padrão
+$ docker logs db_quiz 2>&1 | grep GENERATED
 
-docker exec -it db_quiz mysql -uroot -p
+# Copie a linha que está criptografada em hash, digite o comando abaixo e cole a senha
+$ docker exec -it db_quiz mysql -uroot -p
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+# Depois de acessar o banco dever[a ser criado um usuário
+$ ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
 
-CREATE DATABASE db_quiz;
+# Após isso deve ser criado o banco com o comando abaixo
+$ CREATE DATABASE db_quiz;
 
-UPDATE mysql.user SET host='%' WHERE user='root';
-FLUSH PRIVILEGES;
+# Depois de criado o banco é necessário dar privilégio de acesso para o usuário root
+$ UPDATE mysql.user SET host='%' WHERE user='root';
+$ FLUSH PRIVILEGES;
 
-show databases;
-use db_quiz;
-show tables;
-show columns from db_quiz.users;
-show columns from db_quiz.quiz;
-select * from db_quiz.users, db_quiz.quiz;
+# Alguns comando mais corriqueiros logo abaixo, listar bancos de dados
+$ show databases;
+
+# selecionar o banco para serem feitas consultas
+$ use db_quiz;
+
+# Mostrar as tabelas do banco selecionado
+$ show tables;
+
+# Mostrar a coluna users
+$ show columns from db_quiz.users;
+
+# Mostrar a coluna quiz
+$ show columns from db_quiz.quiz;
+
+# Mostrar os dados contidos nas tabelas quiz e users do banco db_quiz
+$ select * from db_quiz.users, db_quiz.quiz;
